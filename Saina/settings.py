467cba17a2033dac28917ec44a29e9961dfabd41
Mahdi Namaki <mavenium@gmail.com>
2021-02-14 08:53:30 +0330

    add STATIC files settings

--- Saina/settings.py
+++ Saina/settings.py
@@ -128,4 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/assets'),
+]
