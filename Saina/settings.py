62e23a55250d48f6805401849b31dd91bd04322e
Mahdi Namaki <mavenium@gmail.com>
2021-02-15 16:27:26 +0330

    add media settings

--- Saina/settings.py
+++ Saina/settings.py
@@ -133,3 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/assets'),
 ]
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
