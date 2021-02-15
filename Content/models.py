a6fa3befdfd0e8c93963e4055aa5b347daec0dfa
Mahdi Namaki <mavenium@gmail.com>
2021-02-15 16:45:31 +0330

    add SainaLink model

--- Content/models.py
+++ Content/models.py
@@ -1,3 +1,46 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from extensions.custom_validations import image_validation_by_size
+
+
+class SainaLink(models.Model):
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=128,
+        null=False,
+        blank=False
+    )
+    description = models.CharField(
+        verbose_name=_('Description'),
+        max_length=265,
+        null=False,
+        blank=False
+    )
+    slug = models.SlugField(
+        verbose_name=_('Slug'),
+        max_length=32,
+        null=False,
+        blank=False,
+        unique=True
+    )
+    logo = models.ImageField(
+        verbose_name=_('Logo'),
+        upload_to='logos',
+        default='logos/default_logo.png',
+        help_text=_('Max file size is %(size)d MB.') % {'size': settings.MAX_IMAGE_SIZE},
+        validators=[image_validation_by_size],
+        null=False,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = _("Saina Link")
+        verbose_name = _("Saina Links")
+
+    def get_absolute_url(self):
+        return f'/{self.slug}'
